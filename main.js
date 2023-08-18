6ccb77af88dbac04fc412c97cad4929169afe3c8
walid <aguibw@gmail.com>
2023-08-18 03:53:41 +0100

    Add Project Files

---
+++ main.js
@@ -0,0 +1,17 @@
+let array = ["walid","islam"];
+
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);
+}
+
+
+let age = window.prompt("Hi") ;
+
+console.log(typeof Number(age))
+
+let nam = "walid aguib" ;
+
+let first ;
+
+first = nam.slice(4,7) 
+console.log(first)
